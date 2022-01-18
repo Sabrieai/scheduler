52feada7e607aa8bf92ed5f17b08900039f8da9a
Aisha Sabrie <sabriealt@gmail.com>
2022-01-18 17:42:26 -0500

    Add Styles to DayllistItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,26 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  let full = false;
+  if(props.spots === 0) {
+    full = true;
+  }
+
+  const ItemClass = classNames("day-list__item",{
+   "--selected": props.selected,
+   "--full":full
+  })
+
+  const spaceFree = ItemClass.replace(" ","")
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={spaceFree} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3>{spaceFree}</h3>
     </li>
   );
 }
