5851612178e1ae45e42d1066e3b5678b44822e51
Aisha Sabrie <sabriealt@gmail.com>
2022-01-18 18:17:22 -0500

    Add logic to set spots left in a new format

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -16,10 +16,16 @@ export default function DayListItem(props) {
 
   const spaceFree = ItemClass.replace(" ","")
 
+  const formatSpots = () => { 
+    let message = props.spots ? `${props.spots} spots` : "no spots"
+    return `${message} remaining`
+  }
+
   return (
     <li className={spaceFree} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {/* <h3 className="text--light">{props.spots} spots remaining</h3> */}
+      <h3 lassName="text--light">{props.spots === 1 ? `${props.spots} spot remaining`: formatSpots(props.spots)} </h3>
     </li>
   );
 }
