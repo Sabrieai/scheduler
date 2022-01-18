a504baa6317eafca5d2bae155f03ac42a04586cb
Aisha Sabrie <sabriealt@gmail.com>
2022-01-18 16:27:06 -0500

    Build button and add styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   
+   const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+     
+    //the tests are already handling the callback function that 
+    //props.onClick is the callback for clickable
+    //props.disabled is the callback for disabled
+      //empty unless disabled is passed on
+
+   return (
+     
+     <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>
+     
+     );
+
 }
