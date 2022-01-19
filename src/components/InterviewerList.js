c29f22bc6ab0a289c41c53f8e695f1daf5245672
Aisha Sabrie <sabriealt@gmail.com>
2022-01-19 15:18:49 -0500

    Start building Interviwer list component, link styles and stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "components/InterviewerList.scss"
+
+export default function InterviewListItems (props) {
+
+ 
+  return (
+
+      <section className="interviewers">
+       <h4 className="interviewers__header text--light">Interviewer</h4>
+       <ul className="interviewers__list"></ul>
+      </section>
+
+    );
+
+}
