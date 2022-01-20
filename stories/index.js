8faf1e4a50812fd81c9d791201d1660446d6c35a
Aisha Sabrie <sabriealt@gmail.com>
2022-01-19 19:43:32 -0500

    Add Confirm feature that takes props

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
 
 storiesOf("Button", module)
   .addParameters({
@@ -159,6 +160,11 @@ storiesOf("Button", module)
                          interviewer = {interviewer.name}
                          onEdit={action("onEdit")} 
                          onDelete={action("onDelete")}/>)
+      .add("Confirm",() => <Confirm
+                         message = "Delete the appointment?"
+                         onCancel={action("onCancel")} 
+                         onConfirm={action("onConfirm")}/>)
+      
       
 
 
