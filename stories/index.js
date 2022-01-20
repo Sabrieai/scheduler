603fd6db3264a2bc8aefdf1a48fa58e39a0681d4
Aisha Sabrie <sabriealt@gmail.com>
2022-01-19 19:32:35 -0500

    Add Show feature that takes props

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 storiesOf("Button", module)
   .addParameters({
@@ -135,6 +136,7 @@ storiesOf("Button", module)
           onChange={action("setInterviewer")}
         />
       ));
+      
 
       storiesOf("Appointment", module)
       .addParameters({
@@ -152,6 +154,11 @@ storiesOf("Button", module)
       .add("Header", () => (<Header
         time="12pm"/>))
       .add("Empty", () => (<Empty onAdd={action("onAdd")}/>))
+      .add("Show",() => <Show 
+                         student="Lydia Miller-Jones" 
+                         interviewer = {interviewer.name}
+                         onEdit={action("onEdit")} 
+                         onDelete={action("onDelete")}/>)
       
 
 
