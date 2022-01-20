9023dd8127050e609684a3be06411562b9c7ad98
Aisha Sabrie <sabriealt@gmail.com>
2022-01-19 20:07:15 -0500

    Add Error feature that takes props

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
 
 storiesOf("Button", module)
   .addParameters({
@@ -166,6 +167,9 @@ storiesOf("Button", module)
                          onCancel={action("onCancel")} 
                          onConfirm={action("onConfirm")}/>)
       .add("Status", () => <Status message="Deleting"/>)
+      .add("Error", () => <Error 
+                           message="Could not delete appointment."
+                           onClose={action("onClose")}/>)
                       
       
       
