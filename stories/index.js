8e876a9074da30ca64ae5d7c8ea3ae4a3119a2ff
Aisha Sabrie <sabriealt@gmail.com>
2022-01-19 21:25:33 -0500

    Add Form feature that takes props, onSave still needs work

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
 import Error from "components/Appointment/Error"
+import Form from "components/Appointment/From"
 
 storiesOf("Button", module)
   .addParameters({
@@ -170,6 +171,17 @@ storiesOf("Button", module)
       .add("Error", () => <Error 
                            message="Could not delete appointment."
                            onClose={action("onClose")}/>)
+      .add("Create",() => <Form 
+                           interviewers={interviewers}
+                           onSave={action("onSave")}
+                           onCancel={action("onCancel")} />)
+      .add("Edit", () => <Form 
+                          student= "Lydia Miller-Jones"
+                          interviewer={1}
+                          interviewers={interviewers}
+                          onSave={action("onSave")}
+                          onCancel={action("onCancel")}/>)
+                    
                       
       
       
